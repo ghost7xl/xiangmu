a2ac00a7fb8dd3660cfe2337a523fe695a516208
hsgo1234 <33282109+hsgo1234@users.noreply.github.com>
2019-01-05 15:50:13 +0800

    update by hht
    add Goods

---
+++ src/com/shopping/servlet/LoginServlet.java
@@ -0,0 +1,54 @@
+package com.shopping.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.shopping.pojo.User;
+import com.shopping.service.UserService;
+import com.shopping.service.impl.UserServiceImpl;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	UserService userService = new UserServiceImpl();     
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username =  request.getParameter("username");
+		String password =  request.getParameter("password");
+		User user = userService.loginUser(username, password);
+		if(user != null){
+			HttpSession session = request.getSession();
+			session.setAttribute("user", user);
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		}else{
+			response.sendRedirect("error.jsp");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
